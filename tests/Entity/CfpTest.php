e3e4754416323a8a15944fe70122dab15db4c5ff
Pauline Vos <pvos88@gmail.com>
2018-06-14 15:13:12 +0200

    Trim trailing slash on event URIs
    Events were being duplicated as their URIs are used as unique
    identifiers, but the same URI might sometimes have a trailing slash and
    sometimes not.
    
    This trims the URI on the entity when setting it.

---
+++ tests/Entity/CfpTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CallingallpapersTest\Api\Entity;
+
+use Callingallpapers\Api\Entity\Cfp;
+
+final class CfpTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function setEventUriTrimsTrailingSlash(): void
+    {
+        $uri = 'https://some/uri';
+
+        $cfp = new Cfp();
+        $cfp->setEventUri($uri . '/');
+
+        self::assertEquals($uri, $cfp->getEventUri());
+    }
+}
